2215ea73193cdb65afd949ca5aac303adb29adaf
bytegriffin <bytegriffin@192.168.1.101>
2018-05-01 18:43:01 +0800

    init

---
+++ eureka-client/src/main/java/com/bytegriffin/EurekaClientApplication.java
@@ -0,0 +1,34 @@
+package com.bytegriffin;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 访问http://192.168.1.101:8762/hello?name=abc
+ * 可以根据返回信息查看本客户端服务是否可用
+ * @author bytegriffin
+ *
+ */
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	private String port;
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam String name) {
+        return "hello "+name+",i am from port:" +port;
+    }
+
+}
---
+++ eureka-server/src/main/java/com/bytegriffin/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.bytegriffin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 访问http://localhost:8761/
+ * 查看状态页面
+ * @author bytegriffin
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
